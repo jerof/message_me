2aa983576735d06c14f965f8f49a5a3493a945f0
Nikhil <nikhilsamuel.89@gmail.com>
2019-01-18 11:40:51 -0500

    Add users resource with users model, bcrypt authentication and seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Samuel", password: "password")
+User.create(username: "Anika", password: "password")
+User.create(username: "Shoba", password: "password")
+User.create(username: "Naveen", password: "password")
+User.create(username: "Camille", password: "password")
